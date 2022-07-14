c2864f62f369475199137f93b7906125935e314a
chagridsada <chagridsada@dollarsign.co>
2022-07-14 17:13:43 +0700

    fix(activemq): update auth config

--- activemq/bin/run.sh
+++ activemq/bin/run.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
-JETTY_REALM_FILE=$ACTIVEMQ_HOME/conf/jetty-realm.properties
-
 export ACTIVEMQ_OPTS="$ACTIVEMQ_OPTS -Djetty.host=0.0.0.0"
 
 ACTIVEMQ_USER=${ACTIVEMQ_USER:-admin}
 ACTIVEMQ_PASSWORD=${ACTIVEMQ_PASSWORD:-admin}
 
-sed -i "s#activemq.username=system#activemq.username=$ACTIVEMQ_USER#" $ACTIVEMQ_HOME/conf/credentials.properties
-sed -i "s#activemq.password=manager#activemq.password=$ACTIVEMQ_PASSWORD#" $ACTIVEMQ_HOME/conf/credentials.properties
+sed -i \
+  -e "s#activemq.username=system#activemq.username=$ACTIVEMQ_USER#" \
+  -e "s#activemq.password=manager#activemq.password=$ACTIVEMQ_PASSWORD#" \
+  -e "s#guest.password=password#activemq.password=$ACTIVEMQ_PASSWORD#" \
+  $ACTIVEMQ_HOME/conf/credentials.properties
 
-echo "${ACTIVEMQ_USER}: ${ACTIVEMQ_PASSWORD}, admin" >$JETTY_REALM_FILE
+echo "${ACTIVEMQ_USER} readwrite" >$ACTIVEMQ_HOME/conf/jmx.access
+echo "${ACTIVEMQ_USER} ${ACTIVEMQ_PASSWORD}" >$ACTIVEMQ_HOME/conf/jmx.password
+echo "${ACTIVEMQ_USER}=${ACTIVEMQ_PASSWORD}" >$ACTIVEMQ_HOME/conf/users.properties
+echo "admins=${ACTIVEMQ_USER}" >$ACTIVEMQ_HOME/conf/groups.properties
+echo "${ACTIVEMQ_USER}: ${ACTIVEMQ_PASSWORD}, admin" >$ACTIVEMQ_HOME/conf/jetty-realm.properties
+
+SIMPLE_AUTH_PLUGIN="<plugins> \
+    <simpleAuthenticationPlugin anonymousAccessAllowed=\"false\"> \
+        <users> \
+            <authenticationUser username=\"${ACTIVEMQ_USER}\" password=\"${ACTIVEMQ_PASSWORD}\" groups=\"users,admins\"/> \
+        </users> \
+    </simpleAuthenticationPlugin> \
+</plugins>"
+
+sed -i \
+  -e "s#\s</broker>#${SIMPLE_AUTH_PLUGIN}</broker>#" \
+  $ACTIVEMQ_HOME/conf/activemq.xml
 
 exec $ACTIVEMQ_HOME/bin/activemq console
